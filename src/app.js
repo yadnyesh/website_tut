b5a6dd9392412da3fab3ab9a7e5e0c204250a533
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-23 20:59:07 +0530

    Database Model

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const routes = require('./routes/main')
 const hbs = require('hbs')
 const mongoose = require('mongoose')
+const Detail = require("./models/Detail")
 
 const app = express()
 
@@ -15,6 +16,32 @@ hbs.registerPartials("views/partials")
 
 mongoose.connect("mongodb://localhost/admin", () => {
     console.log("Successfully connected to the database.")
+    // Detail.create({
+    //     brandName: "YotaByte Tech",
+    //     brandIconUrl: "https://global-uploads.webflow.com/5e157547d6f791d34ea4e2bf/6087f7f9e5ecc6e0633ecbc0_logo-example3.svg",
+    //     links: [
+    //         {
+    //             label: "Home",
+    //             url: "/"
+    //         },
+    //         {
+    //             label: "Services",
+    //             url: "/services"
+    //         },
+    //         {
+    //             label: "Gallery",
+    //             url: "/gallery"
+    //         },
+    //         {
+    //             label: "About",
+    //             url: "/about"
+    //         },
+    //         {
+    //             label: "Contact Us",
+    //             url: "/contactus"
+    //         }
+    //     ]
+    // })
 })
 
 app.get("/", (request, response) => {
