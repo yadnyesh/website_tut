784f3c97d669626e0679f51dfb4814a113ed1d28
yadnyesh <yadnyesh.juvekar@gmail.com>
2022-01-30 11:42:15 +0530

    Read from DB

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { route } = require('express/lib/application')
 const res = require('express/lib/response')
+const async = require('hbs/lib/async')
+const Detail = require('../models/Detail')
 
 const routes = express.Router()
 
-routes.get("/", (req, res) => {
-    res.render("index")
+routes.get("/", async(req, res) => {
+    const details = await Detail.findOne({"_id":"61ed73e69ff9fc70a1addb92"})
+    console.log(details)
+    res.render("index", {
+        details: details
+    })
 })
 
 routes.get("/gallery", (req, res) => {
